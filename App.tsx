0abd1d97e8dd194462a4bc5dea165b278a5912e2
wongil12 <judgevi52@gmail.com>
2024-05-06 22:28:06 +0900

    feat: apply font hooks & dismiss store

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '@@pages/Home';
@@ -10,44 +10,36 @@ import { Text } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isLoadFont, setIsLoadFont] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      await Font.loadAsync({
-        ExtraLight: require('./assets/fonts/AsapCondensed-ExtraLight.ttf'),
-        ExtraLightItalic: require('./assets/fonts/AsapCondensed-ExtraLightItalic.ttf'),
-        Light: require('./assets/fonts/AsapCondensed-Light.ttf'),
-        LightItalic: require('./assets/fonts/AsapCondensed-LightItalic.ttf'),
-        Regular: require('./assets/fonts/AsapCondensed-Regular.ttf'),
-        RegularItalic: require('./assets/fonts/AsapCondensed-Italic.ttf'),
-        Medium: require('./assets/fonts/AsapCondensed-Medium.ttf'),
-        MediumItalic: require('./assets/fonts/AsapCondensed-MediumItalic.ttf'),
-        SemiBold: require('./assets/fonts/AsapCondensed-SemiBold.ttf'),
-        SemiBoldItalic: require('./assets/fonts/AsapCondensed-SemiBoldItalic.ttf'),
-        Bold: require('./assets/fonts/AsapCondensed-Bold.ttf'),
-        BoldItalic: require('./assets/fonts/AsapCondensed-BoldItalic.ttf'),
-        ExtraBold: require('./assets/fonts/AsapCondensed-ExtraBold.ttf'),
-        ExtraBoldItalic: require('./assets/fonts/AsapCondensed-ExtraBoldItalic.ttf'),
-        Black: require('./assets/fonts/AsapCondensed-Black.ttf'),
-        BlackItalic: require('./assets/fonts/AsapCondensed-BlackItalic.ttf'),
-      });
-
-      setIsLoadFont(true);
-    })();
+  const [fontsLoaded] = useFonts({
+    ExtraLight: require('./assets/fonts/AsapCondensed-ExtraLight.ttf'),
+    ExtraLightItalic: require('./assets/fonts/AsapCondensed-ExtraLightItalic.ttf'),
+    Light: require('./assets/fonts/AsapCondensed-Light.ttf'),
+    LightItalic: require('./assets/fonts/AsapCondensed-LightItalic.ttf'),
+    Regular: require('./assets/fonts/AsapCondensed-Regular.ttf'),
+    RegularItalic: require('./assets/fonts/AsapCondensed-Italic.ttf'),
+    Medium: require('./assets/fonts/AsapCondensed-Medium.ttf'),
+    MediumItalic: require('./assets/fonts/AsapCondensed-MediumItalic.ttf'),
+    SemiBold: require('./assets/fonts/AsapCondensed-SemiBold.ttf'),
+    SemiBoldItalic: require('./assets/fonts/AsapCondensed-SemiBoldItalic.ttf'),
+    Bold: require('./assets/fonts/AsapCondensed-Bold.ttf'),
+    BoldItalic: require('./assets/fonts/AsapCondensed-BoldItalic.ttf'),
+    ExtraBold: require('./assets/fonts/AsapCondensed-ExtraBold.ttf'),
+    ExtraBoldItalic: require('./assets/fonts/AsapCondensed-ExtraBoldItalic.ttf'),
+    Black: require('./assets/fonts/AsapCondensed-Black.ttf'),
+    BlackItalic: require('./assets/fonts/AsapCondensed-BlackItalic.ttf'),
   });
 
-  if (isLoadFont) {
+  if (!fontsLoaded) {
     <Text>Loading...</Text>;
   }
 
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    // <Provider store={store}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </Provider>
   );
 }
